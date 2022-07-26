f57afbcba086ec91d8a99d5671b499c2ff624d02
Francisco Garcia Fio <fran_810@hotmail.com>
2022-07-26 20:26:11 -0300

    sumamos arrays en js

--- JS/index.js
+++ JS/index.js
@@ -30,61 +30,62 @@
 
 //juan, no se porque no me funciona el prompt del appellido, por ende no me concatena el nombre completo.
 
-let nombre= (prompt("ingrese su nombre"));
+//let nombre= (prompt("ingrese su nombre"));
 //let apellido= (prompt("ingrese su apellido"));
-let apellido= ("Garcia");
-alert("Bienvenido a nuestra paguina" + nombre)
+//let apellido= ("Garcia");
+//alert("Bienvenido a nuestra paguina" + nombre)
 
 
 //aplicando correccion propuesta por el tutor juna
-let ingreso = -1;
+//let ingreso = -1;
 
 
-while(ingreso != 0){
-    ingreso = parseInt(prompt("Ingrese 1 para ver campos \n Ingrese 2 para ver casas \n Ingrese 3 para ver alquileres \n Ingrese 4 para ver compras \n Ingrese 5 para ver ventas \n Ingrese 6 para ver tasaciones \n"))
-    switch(ingreso){
-        case 0: alert('Saliste del programa')
-            break;
-        case 1: alert('campos')
-            break;
-        case 2: alert('casas')
-            break;
-        case 3: alert('alquileres')
-            break;
-        case 4: alert('compras')
-            break;
-        case 5: alert('ventas')
-            break;
-        case 6: alert('tasaciones')
-            break;
-        default:
-            ingreso = -1;
-            break;
+//}
+
+
+//function multiplicar(numero1,numero2) {
+    //let result= numero1*numero2
+    //return result
+//}
+
+//let result= multiplicar(precio,0.05)
+
+//alert("El valor de la escrituracion es de $" + result)
+
+//let resultado = sumar(result,precio);
+
+//alert("El valor total de la propiedad con escritura es de $" + resultado)
+
+const inmueble = []
+function propiedad(nombre, precio){
+        this.nombre= nombre
+        this.precio= precio
+}
+
+
+const propiedad1= new propiedad("campo","30000");
+const propiedad2= new propiedad("casa","150000");
+const propiedad3= new propiedad("departamento","10000");
+const propiedad4= new propiedad("quinta","90000");
+const propiedad5= new propiedad("local","50000");
+const propiedad6= new propiedad("galpon","450000");
+
+inmueble.push(propiedad1)
+inmueble.push(propiedad2)
+inmueble.push(propiedad3)
+inmueble.push(propiedad4)
+inmueble.push(propiedad5)
+inmueble.push(propiedad6)
+
+let inmuebles = []
+
+for(let i=0; i < inmueble.length; i++){
+    console.log(typeof(inmueble[i]))
+    if(typeof(inmueble[i]) == 'object' ){
+        inmuebles.push(inmueble[i])
     }
 }
 
-//funcion
-
-let precio= 30
-precio=prompt('ingrese valor en pesos de la propiedad a comprar')
-
-function sumar (num1,num2) {
-    let resultado = num1 + num2
-    return resultado
-}
-
-
-function multiplicar(numero1,numero2) {
-    let result= numero1*numero2
-    return result
-}
-
-let result= multiplicar(precio,0.05)
-
-alert("El valor de la escrituracion es de $" + result)
-
-let resultado = sumar(result,precio);
-
-alert("El valor total de la propiedad con escritura es de $" + resultado)
+console.log(inmueble);
 
 
